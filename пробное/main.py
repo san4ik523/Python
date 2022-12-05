dc70b272dfef7c037a3a6c5d973f867db99ba1d0
User <slipter523@yandex.ru>
2022-12-05 20:32:15 +0700

    5 декабря, пробное занятие

---
+++ пробное/main.py
@@ -0,0 +1,21 @@
+# word = "movavi"
+# lst = ["A", "B ", "C", word, 1.4]
+#
+# print(lst[-2])
+# print(word[0:3])
+# print(word[:-3])
+# print(word[-2:])
+#
+# alphabet = "АБВГДЕЁЖЗ"
+# print(alphabet[ : :1])
+# print(alphabet[ : :-1]) # обратный порядок
+# upper case, п - нижний регистор = lower case
+# s = "бОГдан"
+# print (s.upper())
+# print (s.capitalize())
+
+# magic = "abracadabra"
+# print(magic.count("a"))
+
+y = "i love you"
+# print(phrase.split(" "))
